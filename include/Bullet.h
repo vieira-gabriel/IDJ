26362a193f6e60faac2d2cc6bfda10be015432b9
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2019-05-10 19:28:40 -0300

    Bullet created

---
+++ include/Bullet.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Component.h"
+#include "Sprite.h"
+
+using namespace std;
+
+class Bullet : public Component
+{
+private:
+    Vec2 speed;
+    float distanceLeft;
+    int damage;
+
+public:
+    Bullet(GameObject &associated, float angle, float speed, int damage, float maxDistance, string sprite);
+
+    void Update(float dt);
+    void Render();
+    bool Is(string type);
+    int GetDamage();
+};
