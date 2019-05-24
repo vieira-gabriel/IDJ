06a7d09a59edfee290c28c1b459f3cbd40333032
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2019-05-24 09:25:23 -0300

    Add stage state

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "Game.h"
+#include "StageState.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
     Game &game = Game::GetInstance();
-    //srand(time(NULL));
+    StageState *stg = new StageState();
 
     std::cout << "Game created" << std::endl
               << "Starting game" << std::endl;
+    game.Push(stg);
     game.Run();
+
+    cout << "Closing game" << endl;
 
     return 0;
 }
